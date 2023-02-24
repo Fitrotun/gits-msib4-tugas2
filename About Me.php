677c2fbc8dc24ceb34a16ffb8d4e73c07209ea82
Ida Fitrotun Khasanah <109599910+Fitrotun@users.noreply.github.com>
2023-02-24 13:32:56 +0700

    Add files via upload

---
+++ About Me.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Profil</title>
+</head>
+    <body>
+        <div class="kembali">
+            <button style="background-color: rgb(18, 18, 19);"><a href="Home.php"><font color="white">Back</font></a></button>
+        </div>
+        <section class="profil">
+            <h1>Profil</h1>
+            <p><img src="./pp.jpg" width="180px"></p>
+            <h2><?php echo("Ida Fitrotun Khasanah");?></h2>
+            <p>(Mahasiswi Teknik Informatika Universitas Sains Al-Qur'an)</p>
+        <h1></h1>
+        <hr>
+        <h1></h1>
+        <hr>
+        <section class="conten">
+        <p><?php echo("Hi, saya sekarang adalah mahasiswi Universitas Sains Al-Qur'an Jawa Tengah Di Wonosobo. Saya tinggal di Mentosari RT 005/RW 002, 
+        Kalialang, Kalibawang, Wonosobo, Jawa Tengah. Saya lahir di Wonosobo, 28 Juli 2002 anak kedua dari tiga bersaudara. 
+        Hobi saya mendengarkan musik dan menonton film, sedari kecil saya senang bersosialisasi sehingga sejak saya SMP saya aktif di berbagai organisasi seperti OSIS, PRAMUKA dan IPNU-IPPNU.
+        Kecintaan saya terhadap organisasi berlanjut sampai kuliah saya bergbung dikepengurusan Himpunan Mahasiswa Teknik Informatika (HIMTI). Pengalaman keorganisasian yang saya peroleh membuat saya memiliki banyak relasi, mengerti tentang aturan-aturan instansi
+        ,melatih manajemen waktu, paham mengenai tata cara administrasi dan mengetahui problem solving.
+        Dengan berorganisasi seseorang dapat berproses dengan mudah sehingga tidak menutup diri dari lingkungan, terutama dalam dunia perkuliahan saya tidak ingin hanya sekedar menjadi mahasiswa kupu-kupu yang hanya pulang pergi kuliah dan mengambis diperkuliahan saja.
+        Saya menginginkan point plus, bahwa kuliah dengan berorganisasi tidak mengurangi nilai akademik, justru mahasiswa yang hebat yaitu mahasiswa yang akademiknya bagus organisasinya bagus.")?>
+        </p>
+        <p><?php echo("Saya pernah menjadi ketua Osis, Bendahara di IPPNU, sekretaris di PAC IPNU-IPPNU dan sekarang menjadi sekrataris di HIMTI.
+        Berbagai jabatan di kepengurusan sudah pernah saya rasakan, bahkan hiruk pikuk ketika saya menjadi ketua osis yang independen, dengan adanya masalah yang sangat rumit dan besar pada waktu itu. Saya lulusan SMA Takhasus Al-qur'an jurusan MIPA, dilihat dari backgroundnya saya tidak cukup mumpuni masuk di program studi teknik informatika ini,
+        karena dulu cita-cita saya ingin menjadi seorang guru kimia atau fisika. Namun melihat perkembangan zaman yang serba teknologi
+        saya berfikir bahwa prospek kinerjanya cukup bagus sehingga saya masuk ke prodi TI melihat dukungan dari keluarga yang percaya bahwa walaupun saya perempuan tetapi saya mampu.
+        Banyak yang beranggapan bahwa perempuan masuk di prodi TI tanpa dibekali skill yang mumpuni mereka tidak akan sanggup. Tetapi saya dibekali dengan niat dan bismillah serta mempunyai relasi yang cukup banyak, insyaAllah literasi yang dimiliki bisa membantu dalam proses perkuliahan.
+        Pada semester 6 ini saya berkesempatan untuk memperdalam soft skill saya melalui studi independen di mitra Gits Indonesia
+        dengan fokus materi di fullstack web developer. Dengan begitu saya dapat mengembangkan materi yang pernah saya terima mengenai
+        pemrograman web di kampus, banyaknya tuntutan selama perkulihan di jurusan IT ini, saya harus survive dengan memulai membiasakan
+        diri dengan koding mengoding supaya dapat menunjang perkuliahan maupun saat bekerja nantinya.")?></p>
+               
+            <hr>
+                <footer>
+                    <h6><?php echo("Copyright &copy; 2023 by Ida Fitrotun Khasanah")?></h6>
+                </footer>
+            </section>
+        </body>
+</html>
